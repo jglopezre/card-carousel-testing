fb9df88b37992630618953b2008fb9f4762777fd
Javier Lopez <javierlopez_85@hotmail.com>
2022-02-13 02:03:44 -0300

    configuration options added

--- script.js
+++ script.js
@@ -1,37 +1,74 @@
 $(document).ready(() => {
 
-    let boxCount = 30
-
-    for (let i = 0; i < (boxCount + 1); i++) {
-        $(`<div class="card"><h1>${i + 1}</h1></div>`).appendTo('#carousel-01')
-    }
+    const cardGenerator = (boxCount) => {
+        for (let i = 0; i < boxCount; i++) {
+            $(`<div class="card"><h1>${i + 1}</h1></div>`).appendTo('#carousel-01')
+        }
+            
+        let cardArr = Array.from($('.card'));
         
-    let cardArr = Array.from($('.card'));
+        cardArr.forEach((d) => {
+            d.style.backgroundColor = `rgb( ${Math.floor(Math.random() * 256)}, ${Math.floor(Math.random() * 256)}, ${Math.floor(Math.random() * 256)})`;
+        });
+    }
     
-    cardArr.forEach((d) => {
-        d.style.backgroundColor = `rgb( ${Math.floor(Math.random() * 256)}, ${Math.floor(Math.random() * 256)}, ${Math.floor(Math.random() * 256)})`;
-    });
-    
-    $('#carousel-01').owlCarousel({
-        loop: false,
-        margin: 15,
-        dots: false,
-        autoplay: true,
-        autoplayTimeout: 2000,
-        responsiveClass: true,
-        responsiveBaseElement: ".wrapper",
-        responsive: {
-            0: {
-                items: 1,
-            },
-            400: {
-                items: 2,
-            },
-            700: {
-                items: 3,
-            }
-        }         
-    }); 
-    
+    $('#action-button-01').click( (element) => {
+        element.preventDefault();
+
+        const $checkDots = $('#dots-check');
+        const $checkNav = $('#nav-check');
+        const $checkDrag = $('#drag-check');
+        const $checkAutoplay = $('#auto-play-check');
+        const $checkRewind = $('#rewind-check');
+        const $checkLoop = $('#loop-check');
+        const $checkCenter = $('#center-check');
+        const $inputField = $('#input-field-01');
+
+        $checkDots.attr('disabled', 'true');
+        $checkNav.attr('disabled', 'true');
+        $checkDrag.attr('disabled', 'true');
+        $checkAutoplay.attr('disabled', 'true');
+        $checkRewind.attr('disabled', 'true');
+        $checkLoop.attr('disabled', 'true');
+        $checkCenter.attr('disabled', 'true');
+        $inputField.attr('disabled', 'true');
+        
+
+
+        cardGenerator( parseInt($inputField.prop('value')) );
+        
+        $('#action-button-01').attr('disabled', 'true'); //Disables action button
+
+        let carousel01 = $('#carousel-01').owlCarousel({
+            margin: 15,
+            center: $checkCenter.prop('checked'),
+            loop: $checkLoop.prop('checked'),
+            nav: $checkNav.prop('checked'),
+            navText: ['PREV', 'NEXT'],
+            dots: $checkDots.prop('checked'),
+            autoplay: $checkAutoplay.prop('checked'),
+            autoplayTimeout: 2000,
+            autoplayHoverPause: true,
+            rewind: $checkRewind.prop('checked'),
+            mouseDrag: $checkDrag.prop('checked'),
+            responsiveClass: true,
+            responsiveBaseElement: ".wrapper",
+            responsive: {
+                0: {
+                    items: 1,
+                    dots: false,
+                },
+                400: {
+                    items: 2,
+                },
+                700: {
+                    items: 3,
+                }
+            }         
+        }); 
+
+    } );
+
+
     
 });
